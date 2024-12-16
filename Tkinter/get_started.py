ec448dbaca21d721f62aa24f54d1b4fdf3f4c048
Waqar <waqar.mumtaz@gmail.com>
2024-12-16 11:30:21 +0500

    Add label, button, and entry usage

--- Tkinter/get_started.py
+++ Tkinter/get_started.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+# import tkinter.ttk as ttk #for themed tkinter widgets
 
 # Window is the main container to hold the other UI elements called widgets in tkinter
 window = tk.Tk()
@@ -17,8 +18,65 @@ Frame |	A rectangular region used to group related widgets or provide padding be
 # Add a label widget to display some text on UI
 greeting = tk.Label(text="Hello, Tkinter")
 
-# Add the label to window using pack
-greeting.pack()
+# Add a label with modified UI
+'''
+Different color names like red,orange, yellow, green, purple are also supported an dthe hex codes are also supported like HTML
+Get more HTML Hex Codes here: https://htmlcolorcodes.com/
+'''
+label = tk.Label(
+    text="Hello, Tkinter",
+    foreground="white",  # Set the text color to white
+    background="#34A2FE"  # Set the background color to black
+)
 
-# Start the Tkinter event loop
+# Shorter form 
+#label = tk.Label(text="Hello, Tkinter", fg="white", bg="black")
+
+label2 = tk.Label(
+    text="Hello, Tkinter",
+    fg="white",
+    bg="black",
+    width=10, #measured in text units
+    height=10 #measured in text units
+)
+
+# Buttons are just like labels but are clickable 
+button = tk.Button(
+    text="Click me!",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+)
+
+# Entry widget is for onle liner or short text inputs like name and email
+# Other properties are similar to teh label and button
+entry = tk.Entry(fg="yellow", bg="blue", width=50)
+
+# sample usage of entry with a label
+name_label = tk.Label(text="Name")
+name_entry = tk.Entry()
+
+# get() function helps us getting the value of entry text entered by the user for further processing 
+name = entry.get()
+
+# delete() function is used to delete a character ir a range  of charcaters from the Entry
+entry.delete(0)
+entry.delete(0, 4) #index 4 is not included and orks just like string slicing
+entry.delete(0, tk.END) #tk.END will delete the text till the end 
+
+# Similarly, we can also insert the text in Entry widget at the index position
+entry.insert(0, "Python")
+
+# Add the widgets to window using pack. It centers the widgets as per its behaviour
+greeting.pack()
+label.pack()
+label2.pack()
+button.pack()
+entry.pack()
+
+name_label.pack()
+name_entry.pack()
+
+# Start the Tkinter event loop. It will manages the interactions of all the widgets and act accordingly
 window.mainloop()
