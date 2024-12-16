45a054eb43f9c3fa1b5af1b724f2e11b3a7fd20e
Waqar <waqar.mumtaz@gmail.com>
2024-12-16 18:03:59 +0500

    Add Text widget usage guide

--- Tkinter/get_started.py
+++ Tkinter/get_started.py
@@ -57,6 +57,11 @@ entry = tk.Entry(fg="yellow", bg="blue", width=50)
 name_label = tk.Label(text="Name")
 name_entry = tk.Entry()
 
+
+'''
+Some of the below modifications should work when triggerd like on a buton click. 
+It should be covered later in the learning process
+'''
 # get() function helps us getting the value of entry text entered by the user for further processing 
 name = entry.get()
 
@@ -68,15 +73,52 @@ entry.delete(0, tk.END) #tk.END will delete the text till the end
 # Similarly, we can also insert the text in Entry widget at the index position
 entry.insert(0, "Python")
 
+'''
+Text is similar to the Entry having all supported functions but with a larger text
+However, usage of get, delete, insert is little different. 
+You have to assume a text box as a table (row, col) of characters for better understanding the modification functions.
+'''
+text_label = tk.Label(text="This is a Text Box Below!")
+text_box = tk.Text()
+
+# Here are some usage of the Text widget modifictions
+'''
+#===============================GET===================================
+text_box.get()                          #will return error
+text_box.get("1.0")                     #return one character at row 1 and col 0
+text_box.get("1.0", "1.5")              #return a text starting from first character of first row till 5th character of first row
+text_box.get("1.0", tk.END)             #return all the text from the text box
+#===============================DELETE================================
+text_box.delete("1.0", tk.END)          #delete works exactly like get
+#===============================INSERT================================
+text_box.insert("1.0", "Hello")         #insert also works similar way
+                                        #   This inserts the word Hello at the beginning of the text box, 
+                                        #   using the same "<line>.<column>" format used by .get() to specify the insertion position
+text_box.insert(tk.END, "Put me at the end!")
+text_box.insert(tk.END, "\nPut me on a new line!")      #\n is also a character 
+
+.insert() will do one of two things:
+#Insert text at the specified position if there’s already text at or after that position.
+#Append text to the specified line if the character number is greater than the index of the last character in the text box.
+
+
+
+'''
+
 # Add the widgets to window using pack. It centers the widgets as per its behaviour
 greeting.pack()
 label.pack()
 label2.pack()
+
 button.pack()
+
 entry.pack()
 
 name_label.pack()
 name_entry.pack()
+
+text_label.pack()
+text_box.pack()
 
 # Start the Tkinter event loop. It will manages the interactions of all the widgets and act accordingly
 window.mainloop()
